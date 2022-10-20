edc28e76820701377b1069089b0a2d5a79dc6f06
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-10-20 19:36:19 +0530

    Converting Charachters Small Into Capital And Captical Into Samll Character

--- ConvteringCharachters.py
+++ ConvteringCharachters.py
@@ -1,12 +1,12 @@
 a=input("Enter Your Single Charachter")
 print("given charachter is",a)
 b=ord(a)-32
-print("Return ConvertedS Charachter",a,"is",chr(b))
+print("Return ConvertedS Capital Charachter",a,"is",chr(b))
 print("Happppyyyyy Endddiinggggg !!!")
 
 #Capitcal Charachter convert into smalll Charachter
 a=input("Enter Your Single Charachter")
 print("given charachter is",a)
 b=ord(a)+32
-print("Return ConvertedS Charachter",a,"is",chr(b))
+print("Return Converted Small Charachter",a,"is",chr(b))
 
