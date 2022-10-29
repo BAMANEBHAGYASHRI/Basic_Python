c5a03dc9cb77e83107465c215cfb0b9221d4083c
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-10-29 20:03:58 +0530

    Built-in Function

---
+++ Built-in_Functions/Bulti-inFunction-Input().py
@@ -0,0 +1,9 @@
+a=int(input("Marks for Marathi- "))
+b=int(input("Marks For Hindi -"))
+c=int(input("Marks For English- "))
+d=int(input("Marks For Maths- "))
+e=int(input("Marks For Science- "))
+Total=a+b+c+d+e
+print("Students's Total Marks-",Total)
+Pre=Total/5
+print("Student's Precentage-",Pre)
