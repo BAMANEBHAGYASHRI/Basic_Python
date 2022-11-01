9a7dc03949dff6a40bb3088f34bce5130f8e5c7a
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-01 23:07:49 +0530

    Elseif-Precentage_Counter

---
+++ Conditional_Statement/ElseIf-Marks_Conuter.py
@@ -0,0 +1,9 @@
+marks=int(input("Enter Your Marks"))
+if(marks>=75 and marks<=90):
+    print("Congoo ! \n Frist class with dist ")
+elif(marks>=50 and marks<=75):
+    print("congoo ! Frist Class...")
+elif(marks>=35 and marks<=50):
+    print("congo ! pass")
+elif(marks>=0 and marks<=35):
+    print("Fail ! never give up...!!")
