450729c43c9102913c7654642da4378f87a66d61
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-02 19:52:47 +0530

    Builtin_Function performed on list data type

---
+++ Non_Primitive_DataTypes/NonPrimitiveDataytpe-List/BuiltinFunction-List.py
@@ -0,0 +1,10 @@
+lst=[1,2,3,4,"Bhagyashri","Shruti",12.56,90.76,True,False,12]
+print("list of elements",lst)
+#type function
+print("type of list",type(lst))
+
+#len function
+print("Length of list",len(lst))
+
+#id function
+print("Memory address of List",id(lst))
