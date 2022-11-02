028f8a09d3f1e98480d4b444b249187331ab619a
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-02 20:04:35 +0530

    Slicing Operation Performed on List

---
+++ Non_Primitive_DataTypes/NonPrimitiveDataytpe-List/Slicing_Operation-list.py
@@ -0,0 +1,9 @@
+lst=[12,23,45,"Bhagyashri","Shruti",True,False,78.09,54.89]
+print("List of Elements-",lst)
+print(lst[-1])       #Print Last elements
+print(lst[:6])       #print only givens elements
+print(lst[1:])      #print 1 index from all elements
+print(lst[2:7])     #prints bwteen values(2....9)
+print(lst[::])      #prints All Elmenets
+print(lst[-7:])     #prints reveseed elements from 7 indexed
+print(lst[::-2])
