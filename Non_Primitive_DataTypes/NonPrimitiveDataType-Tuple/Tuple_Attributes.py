efbf703854900ea11b8281870c265024485fb886
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-06 23:19:20 +0530

    Tuple Attributes

---
+++ Non_Primitive_DataTypes/NonPrimitiveDataType-Tuple/Tuple_Attributes.py
@@ -0,0 +1,9 @@
+Tuple=["Bhagyashri",89,56,34.90,True,89,89,89,89]
+print(Tuple)
+#index
+c=Tuple.index(89)
+print("Indexing of element",c)
+
+#count
+a=Tuple.count(89)
+print("Counting of elemnts",a)
