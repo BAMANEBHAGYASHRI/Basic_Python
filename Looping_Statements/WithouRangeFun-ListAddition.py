1ff0d8c933235360d43321d279e6a2f241efea3b
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-22 19:43:07 +0530

    For Loop With Examples

---
+++ Looping_Statements/WithouRangeFun-ListAddition.py
@@ -0,0 +1,11 @@
+lst=[[12,23,34,45,56],[65,67,87,98,90],[78,66,54,89,55],[43,89,63,25,33],[99,73,96,48,98]]
+t=[]
+for test in lst:
+    # print("lenght of lst",len(i))
+    sum=0
+    for test1 in test:
+        sum=sum+test1
+
+    t.append(sum)
+print(t)
+
