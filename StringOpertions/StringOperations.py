85f169a3c7c270d71150816b42645ab733bbc5b9
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-24 23:50:58 +0530

    String Operations

---
+++ StringOpertions/StringOperations.py
@@ -0,0 +1,8 @@
+a="bhagyashri vijay bamane"
+b="       bhagyashri vijay bamane"
+print("Upper String--",a.upper())
+print("Lower String--",a.lower())
+print("Title String--",a.title())
+print("Find String--",a.find("vijay"))
+print("Replace String--",a.replace("bhagyashri","shruti"))
+print("Whitespace remover String--",a.strip())
