7f7cb8459c91d9e0af87211ff9f02da6fc273bf8
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-28 13:50:35 +0530

    ForElse With Example

---
+++ Looping_Statements/ForElse-DivisibleBy5.py
@@ -0,0 +1,8 @@
+a=[12,34,56,79,90,67,45]
+for i in a:
+    if(i%5==0):
+        print(i)
+        break
+
+else:
+    print("Value does not exist div by 5")
---
+++ Looping_Statements/ForElse-Marks.py
@@ -0,0 +1,7 @@
+marks=[79,99,96,97,95]
+for i in marks:
+    if(i<35):
+        print("Faill")
+        break
+else:
+    print("passs")
