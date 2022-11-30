0f809b640071b67e6bd3c69bb58eded82561c3a6
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-30 22:48:07 +0530

    pratice file

---
+++ Function/pratice.py
@@ -0,0 +1,113 @@
+# lst=[77,43,(2,56,90,78),[88,90,98,66],(23,33,32,22),[24,44,55,31],11,15]
+# for i in lst:
+#     print(i)
+# def simple():
+#     a=20
+#     b=30
+#     c=a+b
+#     print(c)
+# a=10
+# b=40
+# print(a+b)
+# simple(a)
+
+# def add():
+#     a=20
+#     b=60
+#     c=a+b
+#     print(c)
+#
+# a=10
+# b=30
+# c=a+b
+# add()
+# print(c)
+#
+# def add():
+#     global a,b
+#     a=20
+#     b=60
+#     c=a+b
+#     print(c)
+#
+# a=10
+# b=30
+# c=a+b
+# print(c)
+# add()
+
+# def add():
+#     a=20
+#     b=60
+#     print(a+b+c)
+# a=10
+# b=30
+# c=a+b
+# add()
+# print(c)
+
+# def add():
+#     a=20
+#     b=60
+#     print(a+b+c)
+# a=10
+# b=30
+# c=a+b
+# print(c)
+# add()
+# #
+
+
+# def add():
+#     a=20
+#     b=60
+#     x=9000
+#     print(a+b+c)
+# a=10
+# b=30
+# c=a+b
+# print(c)
+# x=a+b
+# add()
+# print(x)
+
+# def add():
+#     global x,c
+#     a=20
+#     b=60
+#     x=9000
+#     print(a+b+c)
+# a=10
+# b=30
+# c=a+b
+# print(c)
+# x=a+b
+# add()
+# print(x)
+
+# def add():
+#     global a,x,c
+#     a=20
+#     b=60
+#     x=9000
+#     print(a+b)
+# a=10
+# b=30
+# c=a+b
+# print(c)
+# add()
+
+
+def add():
+    global a,x,c
+    a=20
+    b=60
+    x=9000
+    print(a+b)
+a=10
+b=30
+c=a+b
+add()
+print(c)
+x=a+c
+print(x)
