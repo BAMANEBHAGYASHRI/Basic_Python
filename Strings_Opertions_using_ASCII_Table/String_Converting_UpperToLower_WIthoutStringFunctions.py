35ed59507d7cc611115f9a2d0dde8794a4256cba
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-11-30 22:29:15 +0530

    String Opertions Using ASCII values

--- Strings_Opertions_using_ASCII_Table/String_Converting_UpperToLower_WIthoutStringFunctions.py
+++ Strings_Opertions_using_ASCII_Table/String_Converting_UpperToLower_WIthoutStringFunctions.py
@@ -1,6 +1,6 @@
 str=input("Enter Your String")
 for i in str:
     if(ord(i)>=65 and ord(i)<=90):
-        print(chr(ord(i)+32),end=" ")
+        print(chr(ord(i)+32),end="")
     if(ord(i)>=97 and ord(i)<=122):
-        print(chr(ord(i)-32),end=" ")
+        print(chr(ord(i)-32),end="")
