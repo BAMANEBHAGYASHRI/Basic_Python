32161e74737182317b7d6da484de2d3578849ed5
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-12-04 00:39:29 +0530

    Function-Passing Arguments And Parameters

---
+++ Function/Function-ArgumentPratameterPassing.py
@@ -0,0 +1,19 @@
+def Addition(a,b):
+    c=a+b
+    return c
+
+
+def reminder(a,b):
+    c=a%b
+    return c
+
+def power(a,b):
+    c=a**b
+    return c
+
+x=Addition(20,10)
+y=reminder(2,5)
+z=power(2,9)
+print("Addition---",x)
+print("Reminader---",y)
+print("Power----",z)
