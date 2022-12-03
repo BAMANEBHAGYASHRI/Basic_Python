1bd3b5086228eb90271fd38a365c0e818dfbe6c4
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-12-04 00:43:54 +0530

    Function-Passing Arguments And Parameters Assignment

---
+++ Function/Function-ArgumentPratameterPassing_MarksAssignmnet.py
@@ -0,0 +1,13 @@
+def marks(a):
+    sum=0
+    for i in a:
+        sum=sum+i
+        pre=sum/5
+    return pre
+
+
+
+
+x=marks([90,89,98,97,95])
+print("Congrattss Youu GOtt Fristt CLAsss Withhh Distincationnn..!!!",x)
+    
