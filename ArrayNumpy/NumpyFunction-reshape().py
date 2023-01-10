23a26d2310532ded4655fdc2cb9807e0f7275d29
BHAGYASHRI <shrutibamaneb@gmail.com>
2023-01-10 08:11:39 +0530

    Array and Numpy Function

---
+++ ArrayNumpy/NumpyFunction-reshape().py
@@ -0,0 +1,10 @@
+import numpy as n
+b=n.arange(8).reshape(2,4)
+print(b)
+print("-----------------------------------")
+c=n.arange(12).reshape(4,3)
+print(c)
+print("-----------------------------------")
+d=n.arange(8).reshape(2,2,2)
+print(d)
+
