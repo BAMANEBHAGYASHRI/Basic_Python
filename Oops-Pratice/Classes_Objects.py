4ddd771d52870332289d1bbb479e949a8f78f519
BHAGYASHRI <shrutibamaneb@gmail.com>
2023-01-12 22:07:26 +0530

    Classes and object example

---
+++ Oops-Pratice/Classes_Objects.py
@@ -0,0 +1,19 @@
+class demo:
+    def method1(self):
+        print("hiiiieeeeeeeeeeeeeeeeeee")
+
+    def method2(self):
+        print("hooowwwwwwwww areeeee youuuuuuuuuuuuuuuuuuu")
+
+    def method3(self):
+        print("gooooddddd morninnnngggggggg")
+
+    def method4(Self):
+        print("gggoooooooooooodddddd byyyyyyyeeeeeeeeeeeeeeeeeee")
+
+
+m1=demo()
+m1.method1()
+#m1.method2()
+#m1.method3()
+m1.method4()
